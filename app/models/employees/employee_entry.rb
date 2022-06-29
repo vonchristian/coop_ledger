03781153a9f9f7f558a785e21caca71e9ad06a54
Von Christian Halip <vc.halip@gmail.com>
2022-06-29 22:14:32 +0800

    Fix zeitwerk issue

--- app/models/employees/employee_entry.rb
+++ app/models/employees/employee_entry.rb
@@ -1,4 +1,7 @@
-class EmployeeEntry < ApplicationRecord
-  belongs_to :entry
-  belongs_to :employee
+# frozen_string_literal: true
+module Employees
+  class EmployeeEntry < ApplicationRecord
+    belongs_to :entry
+    belongs_to :employee
+  end
 end
